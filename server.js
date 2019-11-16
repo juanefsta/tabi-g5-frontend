8fff0c8d7acd5a0c9495f50826df2991c5c4ec97
Juan Efstathopoulos <juanefsta@gmail.com>
2019-11-16 15:17:11 -0300

    First commit

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/tabi-g5'));
+app.use(express.static(__dirname + '/dist/frontend'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/tabi-g5/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/frontend/index.html'));
 });
 
 // default Heroku PORT
